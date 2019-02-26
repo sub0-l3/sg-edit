3aeac7d6fb601896a8589ad8d28b867daa9d91c4
sub0-l3 <subhrajit.debnath@gmail.com>
2019-02-27 00:19:36 +0530

    re-arrange layout & set it up for easy debugging

--- js/index.js
+++ js/index.js
@@ -1,24 +1,20 @@
 let dataPersist = [];
 let data = {};
 
-
 function check() {
-  
   data = {};
   storyPageEl = document.getElementById("storyReader");
-  var bodyRect = document.body.getBoundingClientRect(),
-    elemRect = storyPageEl.getBoundingClientRect(),
-    offset   = elemRect.top - bodyRect.top;
+  let bodyRect = document.body.getBoundingClientRect();
+  // condition to handle case where selection starts from a space
+  let anchorNode = document.getSelection().anchorNode,
+    focusNode = document.getSelection().focusNode;
+  let startIdx = anchorNode.parentNode.attributes["data-cue"]
+    ? anchorNode.parentNode.attributes["data-cue"].value
+    : anchorNode.nextSibling.attributes["data-cue"].value;
 
-console.log('Element is ' + offset + ' vertical pixels from <body>');
-  console.log(bodyRect);
-  console.log(elemRect);
-  
-  startIdx = document.getSelection().anchorNode.parentNode.attributes[
-    "data-cue"
-  ].value;
-  endIdx = document.getSelection().focusNode.parentNode.attributes["data-cue"]
-    .value;
+  endIdx = focusNode.parentNode.attributes["data-cue"]
+    ? focusNode.parentNode.attributes["data-cue"].value
+    : focusNode.previousSibling.attributes["data-cue"].value;
 
   minIdx = Math.min(startIdx, endIdx);
 
@@ -29,14 +25,15 @@ console.log('Element is ' + offset + ' vertical pixels from <body>');
   data.selected = [];
   for (let i = minIdx; i <= maxIdx; i++) {
     let cueEl = document.querySelector(`span[data-cue='${i}']`);
-    
+
     if (cueEl) {
       data.selected.push(cueEl.innerHTML);
-     cueEl.style.backgroundColor = "yellow";
+      elemRect = cueEl.getBoundingClientRect();
+      offset = elemRect.top - bodyRect.top;
+      console.log("Element is " + offset + " vertical pixels from <body>");
     }
   }
-  console.log(data);
-  document.getElementById("popup__comments").innerHTML = `<pre>${JSON.stringify(
+  document.getElementById("popup__data").innerHTML = `<pre>${JSON.stringify(
     data,
     null,
     2
@@ -48,15 +45,16 @@ console.log('Element is ' + offset + ' vertical pixels from <body>');
 function submitComments() {
   var comment = document.getElementById("popup__input").value;
   document.getElementById("popup__input").value = "";
-
+  if (Object.keys(data).length === 0) return;
   data.comment = comment;
   dataPersist.push(data);
   for (let i = data.startIdx; i <= endIdx; i++) {
     let cueEl = document.querySelector(`span[data-cue='${i}']`);
     if (cueEl) {
-     cueEl.style.backgroundColor = "#faeaea";
+      cueEl.style.backgroundColor = "rgb(233, 156, 156)";
     }
   }
+  data = {};
   document.getElementById("data-persist").innerHTML = `<pre>${JSON.stringify(
     dataPersist,
     null,
@@ -64,18 +62,15 @@ function submitComments() {
   )}<pre>`;
 }
 
-// Listen to all clicks on the document
-document.addEventListener(
-  "mouseover",
-  function(event) {
-    // If the event target doesn't match bail
-    if (!event.target.hasAttribute("data-cue")) return;
-    currentIdx = event.target.attributes["data-cue"].value;
-    // Otherwise, run your code...
-    let comments = dataPersist.filter(data => currentIdx >= data.startIdx && currentIdx <= data.endIdx);
-    console.log(comments);
-    let commentsEl = comments.map(c => `<li>${c.comment}</li>`);
-   console.log(commentsEl); document.getElementById("popup__comments").innerHTML = commentsEl.join("");
-  },
-  false
-);
+// Listen to all mouseover events on the document
+document.addEventListener("mouseover", function(event) {
+  // If the event target doesn't match bail
+  if (!event.target.hasAttribute("data-cue")) return;
+  currentIdx = event.target.attributes["data-cue"].value;
+  // Otherwise, run your code...
+  let comments = dataPersist.filter(
+    data => currentIdx >= data.startIdx && currentIdx <= data.endIdx
+  );
+  let commentsEl = comments.map(c => `<li>${c.comment}</li>`);
+  document.getElementById("popup__comments").innerHTML = commentsEl.join("");
+});
